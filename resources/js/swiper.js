23e50f4ac604340fcf54d70becec1a223ec11d55
Amiory <amiory05@gmail.com>
2024-09-25 18:33:24 +0330

    ok

--- resources/js/swiper.js
+++ resources/js/swiper.js
@@ -1,14 +1,43 @@
-if (window.matchMedia("(min-width:768px)").matches) {
-} else {
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "horizontal",
-    loop: true,
 
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+
+  // const swiper = new Swiper(".swiper", {
+  //   // Optional parameters
+  //   direction: "horizontal",
+  //   loop: true,
+  //   spaceBetween: 50,
+
+  //   // If we need pagination
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   // Navigation arrows
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  // });
+
+
+const productSwiper = new Swiper(".productSwiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  spaceBetween: 20,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
-  });
-}
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    }
+  },
+});
