56fc0b9c487ac8fb3b1bebbd84eb493bebe2442e
Amiory <amiory05@gmail.com>
2024-09-28 12:59:06 +0330

    faq

--- resources/js/swiper.js
+++ resources/js/swiper.js
@@ -1,35 +1,36 @@
+if (window.matchMedia("(min-width: 768px)").matches) {
+} else {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
 
-
-  // const swiper = new Swiper(".swiper", {
-  //   // Optional parameters
-  //   direction: "horizontal",
-  //   loop: true,
-  //   spaceBetween: 50,
-
-  //   // If we need pagination
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //     clickable: true,
-  //   },
-  //   // Navigation arrows
-  //   navigation: {
-  //     nextEl: ".swiper-button-next",
-  //     prevEl: ".swiper-button-prev",
-  //   },
-  // });
-
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
 
 const productSwiper = new Swiper(".productSwiper", {
   // Optional parameters
   direction: "horizontal",
   loop: false,
+  // simulateTouch: false,
+  // touchEventsTarget: "container",
   spaceBetween: 20,
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  
+
   breakpoints: {
     768: {
       slidesPerView: 3,
@@ -38,6 +39,6 @@ const productSwiper = new Swiper(".productSwiper", {
     1024: {
       slidesPerView: 3,
       spaceBetween: 50,
-    }
+    },
   },
 });
